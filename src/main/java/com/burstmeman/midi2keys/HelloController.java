74d3d81a502c245bf8c9cbd4539d0772ca03bcdd
burstmeman <burstmeman@pm.me>
2026-01-07 14:33:14 +0100

    feat: init project structure

---
+++ src/main/java/com/burstmeman/midi2keys/HelloController.java
@@ -0,0 +1,14 @@
+package com.burstmeman.midi2keys;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
